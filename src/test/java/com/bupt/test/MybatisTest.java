cf4cf86db3c24ba6211d18b705682bbcecea7616
hankang370 <hankang370@bupt.edu.cn>
2020-09-23 09:28:39 +0800

    Initial commit

---
+++ src/test/java/com/bupt/test/MybatisTest.java
@@ -0,0 +1,39 @@
+package com.bupt.test;
+
+import com.bupt.dao.IUserDao;
+import com.bupt.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.lf5.util.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * mybatis的入门案例
+ */
+public class MybatisTest {
+    public static void main(String[] args) throws Exception {
+        // 1.读取配置文件
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 2.创建SqlSessionFactory工厂--构建者模式
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(in);
+        // 3.使用工厂生产SqlSession--工厂模式
+        SqlSession session = factory.openSession();
+        // 4.使用SqlSession创建Dao接口的代理对象
+        IUserDao userDao = session.getMapper(IUserDao.class);
+        // 5.使用代理对象执行方法
+        List<User> users = userDao.findAll();
+        for(User user : users){
+            System.out.println(user);
+        }
+        // 6.释放资源
+        session.close();
+        in.close();
+    }
+
+}
